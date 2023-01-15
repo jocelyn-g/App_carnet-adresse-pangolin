462f558ee5fe7c850e9771b6310d9c865d7d498e
Jocelyn GEZ <jocelyn.gez@gmail.com>
2023-01-15 21:43:50 +0100

    ajout des option add et delete pour la list contact des users

--- src/app/public/contact/contact.component.ts
+++ src/app/public/contact/contact.component.ts
@@ -1,10 +1,79 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ICredential2 } from 'src/app/_interfaces/credential2';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit{
+
+  userList: any[] = []
+
+  self: ICredential2 = {
+    email: "",
+    password: "",
+    name: "",
+    role: "",
+    contact: [],
+  }
+  id: any
+
+  constructor(private http: HttpClient){}
+
+  ngOnInit(): void {
+    this.http.get('http://localhost:3000/posts/').subscribe(
+      (users: any) => {
+        console.log(users[0])
+        this.userList = users
+
+        let identify = 0
+        let count = 0
+        for(let user of users){
+          let emaillocal = localStorage.getItem('email')
+          if(user.email == emaillocal){
+            identify = count
+          }else{
+            count = count + 1
+          }
+        }
+        console.log(users[identify])
+        this.id = users[identify]._id
+        console.log(this.id)
+        this.self.name = users[identify].name,
+        this.self.role = users[identify].role,
+        this.self.email = users[identify].email,
+        this.self.password = users[identify].password,
+        this.self.contact = users[identify].contact
+      },
+    )
+  }
+
+  getContact = 0
+  increment = 0
+  btnDelete(getId:any){
+    this.increment = 0
+    console.log(getId)
+      //this.self.contact.push(getId)
+      console.log(this.self)
+      for(let index of this.self.contact){
+        if(index !== getId){
+          this.increment = this.increment + 1
+        }else{
+          this.getContact = this.increment
+        }
+      }
+      console.log(`l'index est ${this.getContact}`)
+      this.self.contact.splice(this.getContact, 1)
+      console.log(this.self.contact)
+
+      this.http.put('http://localhost:3000/posts/'+this.id, this.self).subscribe(
+      (newUsers: any) => {
+        newUsers = this.self
+      },
+    )
+    
+  }
 
 }
--- src/app/public/user/user.component.ts
+++ src/app/public/user/user.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ICredential2 } from 'src/app/_interfaces/credential2';
 
 @Component({
   selector: 'app-user',
@@ -10,19 +11,60 @@ export class UserComponent implements OnInit{
 
   userList: any[] = []
 
+  self: ICredential2 = {
+    email: "",
+    password: "",
+    name: "",
+    role: "",
+    contact: [],
+  }
+  id: any
+
   constructor(private http: HttpClient){}
 
   ngOnInit(): void {
     this.http.get('http://localhost:3000/posts/').subscribe(
       (users: any) => {
-        console.log(users)
+        //console.log(users[0])
         this.userList = users
+
+        let identify = 0
+        let count = 0
+        for(let user of users){
+          let emaillocal = localStorage.getItem('email')
+          if(user.email == emaillocal){
+            identify = count
+          }else{
+            count = count + 1
+          }
+        }
+        //console.log(users[identify])
+        this.id = users[identify]._id
+        //console.log(this.id)
+        this.self.name = users[identify].name,
+        this.self.role = users[identify].role,
+        this.self.email = users[identify].email,
+        this.self.password = users[identify].password,
+        this.self.contact = users[identify].contact
       },
     )
   }
 
-  onClick(){
-    console.log()
+  btnAdd(getId:any){
+    //console.log(getId)
+    if(!this.self.contact.includes(getId)){
+      this.self.contact.push(getId)
+      //console.log(this.self)
+
+      this.http.put('http://localhost:3000/posts/'+this.id, this.self).subscribe(
+      (newUsers: any) => {
+        newUsers = this.self
+      },
+    )
+    }else{
+      //console.log("déja ajouter")
+    }
+    
   }
 
 }
